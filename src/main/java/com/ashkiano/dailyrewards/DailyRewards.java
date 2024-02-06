cf635d3654ad3282a5d4066bde02f4e370cbf891
jvyskocil <ash.kiano@gmail.com>
2024-02-06 11:17:02 +0100

    version 1.1

--- src/main/java/com/ashkiano/dailyrewards/DailyRewards.java
+++ src/main/java/com/ashkiano/dailyrewards/DailyRewards.java
@@ -7,6 +7,7 @@ public class DailyRewards extends JavaPlugin {
     public void onEnable() {
         saveDefaultConfig();
         getCommand("daily").setExecutor(new DailyCommandExecutor(this));
+        this.getLogger().info("Thank you for using the DailyRewards plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://donate.ashkiano.com");
     }
 
     @Override
